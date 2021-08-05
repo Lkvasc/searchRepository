af681ba4d675539f588081f33fedeafcb3d66938
Lucas Vasconcelos <lucas.vasconcelos@ax4b.com>
2021-08-04 22:13:36 -0300

    Adicionando rotas utilizando react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import React, { useState } from 'react';
-import axios from 'axios';
+import Routes from './routes';
 
-function App(props) {
-  const [usuario, setUsuario ] = useState('');
+function App() {
 
-  function handlePesquisa()  {
-      axios.get(`https://api.github.com/users/${usuario}/repos`).then(
-        response => console.log(response.data)
-      );
-  }
   return (
     <>
-      <p> {usuario}</p>
-      <input className="usuarioInput" placeholder="Usuário" value={usuario} onChange={ e => setUsuario(e.target.value)} />
-      <button type="button" onClick={handlePesquisa}>Pesquisar</button>    
+      <Routes/>
     </>
-    
   );
 }
  
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Repositories from './Repositories';
 
 
 ReactDOM.render(
