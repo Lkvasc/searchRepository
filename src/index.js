545da4e3734a5240706cbf917f1e14cb446d5573
Lucas Vasconcelos <lucas.vasconcelos@ax4b.com>
2021-08-05 20:59:52 -0300

    Style components creation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Repositories from './Repositories';
-
 
 ReactDOM.render(
     <App title="Hello Gama Academy" user="Gama Academy" />,
